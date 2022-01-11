0855e1f1cf2fbb69ba687c09fd1c8dd80fe81e72
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-01-11 14:54:30 -0600

    anchor tags added to followers

--- src/App.js
+++ src/App.js
@@ -14,23 +14,23 @@ class App extends React.Component {
         <a href="https://github.com/CaitlinTalbert"><h3>Caitlin Talbert</h3></a>
         <p>(CaitlinTalbert)</p>
         <p>Total Repositories: 34 </p>
-        <p>Total Followers: 5</p>
+        <p>Total Followers: 6</p>
       </div>
 
       <div id="followers">
         <h3>Followers:</h3>
         <img src="https://avatars.githubusercontent.com/u/38574400?v=4"/>
-        <p>dbvker</p>
+        <a href="https://github.com/dbvker"><p>dbvker</p></a>
         <img src="https://avatars.githubusercontent.com/u/77358128?v=4"/>
-        <p>PriscilaMonteiro</p>
+        <a href="https://github.com/PriscilaMonteiro"><p>PriscilaMonteiro</p></a>
         <img src="https://avatars.githubusercontent.com/u/91200504?v=4"/>
-        <p>JorgeEvangelista500</p>
+        <a href="https://github.com/JorgeEvangelista500"><p>JorgeEvangelista500</p></a>
         <img src="https://avatars.githubusercontent.com/u/91918476?v=4"/>
-        <p>daaavit</p>
+        <a href="https://api.github.com/users/daaavit"><p>daaavit</p></a>
         <img src="https://avatars.githubusercontent.com/u/92267062?v=4"/>
-        <p>kfam22</p>
+        <a href="https://github.com/kfam22"><p>kfam22</p></a>
         <img src="https://avatars.githubusercontent.com/u/92415478?v=4"/>
-        <p>BrittanyPete</p>
+        <a href="https://github.com/BrittanyPete"><p>BrittanyPete</p></a>
       </div>
     </div>);
   }
