45d8881ba9dfd74de84356f4d2ac3c3330ccd1c8
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-01-11 14:25:48 -0600

    basic DOM complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   render() {
     return(<div>
-      Github Card
+      <h1>GitHub Info</h1>
+      <form>
+        <input />
+        <button>Search</button>
+      </form>
+      <div>
+        <img src="https://api.github.com/users/CaitlinTalbert"/>
+      </div>
+      <div>
+        <h3>Followers:</h3>
+        <img src="https://api.github.com/users/your_github_name/followers"/>
+      </div>
     </div>);
   }
 }
