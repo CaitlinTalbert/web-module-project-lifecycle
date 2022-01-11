b3a6c7a33449926ed9e4b58e2dd19a8b843478da
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-01-11 14:49:08 -0600

    DOM updated with images of user and followers

--- src/App.js
+++ src/App.js
@@ -9,12 +9,28 @@ class App extends React.Component {
         <input />
         <button>Search</button>
       </form>
-      <div>
-        <img src="https://api.github.com/users/CaitlinTalbert"/>
+      <div id="UserCard">
+        <img src= "https://avatars.githubusercontent.com/u/92820628?v=4"/>
+        <a href="https://github.com/CaitlinTalbert"><h3>Caitlin Talbert</h3></a>
+        <p>(CaitlinTalbert)</p>
+        <p>Total Repositories: 34 </p>
+        <p>Total Followers: 5</p>
       </div>
-      <div>
+
+      <div id="followers">
         <h3>Followers:</h3>
-        <img src="https://api.github.com/users/your_github_name/followers"/>
+        <img src="https://avatars.githubusercontent.com/u/38574400?v=4"/>
+        <p>dbvker</p>
+        <img src="https://avatars.githubusercontent.com/u/77358128?v=4"/>
+        <p>PriscilaMonteiro</p>
+        <img src="https://avatars.githubusercontent.com/u/91200504?v=4"/>
+        <p>JorgeEvangelista500</p>
+        <img src="https://avatars.githubusercontent.com/u/91918476?v=4"/>
+        <p>daaavit</p>
+        <img src="https://avatars.githubusercontent.com/u/92267062?v=4"/>
+        <p>kfam22</p>
+        <img src="https://avatars.githubusercontent.com/u/92415478?v=4"/>
+        <p>BrittanyPete</p>
       </div>
     </div>);
   }
